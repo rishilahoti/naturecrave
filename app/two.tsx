422a79730800adb0d6e907031689eddeb765e449
rishilahoti <rishilahoti99@gmail.com>
2025-02-04 00:58:54 +0530

    initual

---
+++ app/two.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Two() {
+    return (
+        <div className="container">
+            <Image src="/2.png" alt="first page" width={1920} height={1080} />
+        </div>
+    );
+}
